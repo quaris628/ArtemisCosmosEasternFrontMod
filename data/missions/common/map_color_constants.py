d475c37e3ebf671cf1e34f921eafe6ae99201b70
Quaris <quaris314@gmail.com>
2026-06-10 00:04:41 -0500

    EF mod: Support pirate and neutral civilian stations - Color them dark green (instead of the default dark blue)

---
+++ data/missions/common/map_color_constants.py
@@ -0,0 +1,12 @@
+
+# ================================
+#  Pirates
+# ================================
+
+# _lib/procedural_terrain.py
+
+def color_map_pirate_station():
+    return "#036910"
+
+def color_map_neutral_civilian_station():
+    return "#15f"
