e89b54e47eaa30106f2d5939802bdcaf626ef4c6
Quaris <quaris314@gmail.com>
2026-06-10 00:25:06 -0500

    EF mod: Make titles of comms messages from npcs match their colors on the map (maybe later split out the vanilla-compatible stuff into a separate commit)

--- data/missions/common/map_color_constants.py
+++ data/missions/common/map_color_constants.py
@@ -1,4 +1,8 @@
 
+from sbs_utils.procedural.roles import has_role
+
+from data.missions.common.pirate_features_definitions import is_neutral_civilian, is_pirate, is_tsn
+
 # ================================
 #  Pirates
 # ================================
@@ -15,3 +19,27 @@ def color_map_neutral_civilian_station():
 
 def color_map_pirate_npc():
     return "#036910"
+
+# ================================
+#  Misc
+# ================================
+
+def get_comms_message_title_color(npc_id):
+    if is_tsn(npc_id):
+        if has_role(npc_id, "station"):
+            # from preferences.json
+            # "gui-color-friendly-station": "#15f",
+            return "#15f"
+        else:
+            # from preferences.json
+            # "gui-color-friendly-ship": "#0ff",
+            return "#0ff"
+    elif is_pirate(npc_id):
+        if has_role(npc_id, "station"):
+            return color_map_pirate_station()
+        else:
+            return color_map_pirate_npc()
+    elif is_neutral_civilian(npc_id):
+        return color_map_neutral_civilian_station()
+    else:
+        return None
